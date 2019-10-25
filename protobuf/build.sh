97edb7b71a011489e73f1d50505d37f7b664cba9
Oleh Kolomiitsev <OKolomiitsev@luxoft.com>
2019-10-25 17:30:04 +0300

    Genereate protobuf sources during binary build.

--- protobuf/build.sh
+++ protobuf/build.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+PROTOFILE=AMessage.proto
+PLUGIN=protoc-gen-c
+PROTO_OUT=./
+PROTO_PATH=./
+
+echo "Protofiles:$PROTOFILE"
+echo "Plugin:$PLUGIN"
+echo "Proto out:$PROTO_OUT"
+echo "Proto path:$PROTO_PATH"
+echo "[*] About to generate protobuf..."
+protoc --plugin=$PLUGIN --proto_path=$PROTO_PATH --c_out=$PROTO_OUT $PROTOFILE
+
 OUT=test
-echo "[*] About to build...$OUT"
+echo "[*] About to build binary:$OUT"
 gcc ./main.c -o $OUT
--- protobuf/clean.sh
+++ protobuf/clean.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-BINARY=test
-echo "[*] About to clean...$BINARY"
-rm ./$BINARY
+TEST_BINARY=test
+GENERATED_HEADERS=*.pb-c.h
+GENERATED_SOURCES=*.pb-c.c
+
+echo "[*] About to remove binary:$TEST_BINARY"
+rm ./$TEST_BINARY
+echo "[*] About to remove protobuf generated headers:$GENERATED_HEADERS"
+rm ./$GENERATED_HEADERS
+echo "[*] About to remove protobuf generated headers:$GENERATED_SOURCES"
+rm ./$GENERATED_SOURCES
