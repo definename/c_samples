4bc78ea638052cab7151f0de576890d0a80442fa
Oleh Kolomiitsev <OKolomiitsev@luxoft.com>
2019-10-25 18:54:09 +0300

    Failed to build.

--- protobuf/main.c
+++ protobuf/main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "AMessage.pb-c.h"
 
 int main()
 {
+    // AMessage *msg = NULL;
+    // msg = malloc(sizeof(*msg));
+    // amessage__init(msg);
+
     printf("It works!!!\n");
     return 0;
 }
