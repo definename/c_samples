dd2d1d75977bc7933bd650021090f57f5af0c3f2
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2019-10-27 22:04:38 +0200

    Makefile fixes.

--- protobuf/main.c
+++ protobuf/main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-// #include <stdlib.h>
-// #include "AMessage.pb-c.h"
+#include <stdlib.h>
+#include "AMessage.pb-c.h"
 
 int main()
 {
-    // AMessage *msg = NULL;
-    // msg = malloc(sizeof(*msg));
-    // amessage__init(msg);
-
+    AMessage *msg = NULL;
+    msg = malloc(sizeof(*msg));
+    amessage__init(msg);
+    
     printf("It works!!!\n");
+
     return 0;
 }
