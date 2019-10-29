e0e399a89cf32aed52ef1311e7bab6ef354d341c
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2019-10-29 23:10:43 +0200

    Swap using c pointers.

--- cptr/main.c
+++ cptr/main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+void my_swap(int *a, int *b);
+void test_swap(void);
 
 int main (int argc, const char *argv[]){
-  printf("It works!!!\n");
+  test_swap();
   return 0;
+}
+
+void test_swap(void){
+  int a = 0;
+  int b = 1;
+  printf("a:%d b:%d\n", a, b);
+  my_swap(&a, &b);
+  printf("a:%d b:%d\n", a, b);
+}
+
+void my_swap(int *a, int *b){
+  int temp = *a;
+  *a = *b;
+  *b = temp;
 }
