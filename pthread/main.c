c3c32d4a62f9ffee61519115a264082bfe4e6f0f
Oleh Kolomiitsev <OKolomiitsev@luxoft.com>
2019-10-29 16:51:55 +0200

    memset test.

--- pthread/main.c
+++ pthread/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "pthread.h"
+// Test
 #include <stdbool.h>
+#include <string.h>
 
 void *t_handler(void* t_data){
   printf("We are here!!!\n");
@@ -12,6 +14,19 @@ void test_bool(const bool ret){
   printf("%s\n", ret ? "true" : "false");
 }
 
+typedef struct{
+  int a;
+  int b; 
+} test_struct_t;
+
+void test_memset(void){
+  test_struct_t test_struct;
+  printf("%d %d\n", test_struct.a, test_struct.b);
+  memset(&test_struct, 0, sizeof(test_struct_t));
+  printf("%d %d\n", test_struct.a, test_struct.b);
+}
+
+
 int main(){
   void* t_data = NULL;
   pthread_t t;
@@ -19,7 +34,16 @@ int main(){
 
   pthread_join(t, NULL);
 
+  // .....................................................
   test_bool(true);
   test_bool(false);
+  bool b;
+  if (b){
+    printf("111\n");
+  }
+
+  // .....................................................
+  test_memset();
+
   return 0;
 }
