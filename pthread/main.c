19e10d84e467ed0ce1e49bb6cd4f3b1d1ba0e0a6
Oleh Kolomiitsev <OKolomiitsev@luxoft.com>
2019-10-29 12:22:13 +0200

    test ternary operator in c.

--- pthread/main.c
+++ pthread/main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "pthread.h"
+#include <stdbool.h>
 
 void *t_handler(void* t_data){
   printf("We are here!!!\n");
   pthread_exit(0);
+}
+
+void test_bool(const bool ret){
+  printf("%s\n", ret ? "true" : "false");
 }
 
 int main(){
@@ -13,5 +18,8 @@ int main(){
   pthread_create(&t, NULL, t_handler, t_data);
 
   pthread_join(t, NULL);
+
+  test_bool(true);
+  test_bool(false);
   return 0;
 }
