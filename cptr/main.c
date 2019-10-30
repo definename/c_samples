3223d41fb93c25033b17482a4d8a55b38c54ec87
Oleh Kolomiitsev <OKolomiitsev@luxoft.com>
2019-10-30 11:18:06 +0200

    c function pointer alias.

--- cptr/main.c
+++ cptr/main.c
@@ -1,17 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 void my_swap(int *a, int *b);
 void test_swap(void);
 
 void test_array(void);
 
+void test_func_ptr(void);
+
+typedef void(*request_done_t)(bool ok);
+
+typedef struct{
+  int data;
+  request_done_t done;
+} request_t;
+
 int main (int argc, const char *argv[]){
 
   test_swap();
   test_array();
+  test_func_ptr();
 
   return 0;
+}
+
+// .......................................................
+
+void request_done_handler(bool ok){
+  printf("request done handler:%d\n", ok);
+}
+
+void test_func_ptr(void){
+  request_t *req = (request_t *)malloc(sizeof(request_t));
+  if (req){
+    req->data = 99;
+    req->done = request_done_handler;
+  }
+  req->done(true);
+  req->done(false);
 }
 
 // .......................................................
