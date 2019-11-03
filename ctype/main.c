81b43f4713c2aebdab8d230331602e62630797a4
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2019-11-03 20:03:51 +0200

    c portable types.

--- ctype/main.c
+++ ctype/main.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
+#include <inttypes.h>
 
 void test_bases(void);
 void test_sscanf(void);
+void test_portable_type(void);
 
 int main (int argc, const char *argv[]){
   test_sscanf();
   test_bases();
+  test_portable_type();
   return 0;
+}
+
+void test_portable_type(void){
+  int32_t i = 9;
+  printf("PRId32:%" PRId32 "\n", i);
+  int_least32_t ii = 99;
+  printf("PRIdLEAST32:%" PRIdLEAST32 "\n", ii);
+  int_fast32_t iii = 999;
+  printf("PRIdFAST32:%" PRIdFAST32 "\n", iii);
 }
 
 void test_bases(void){
