58855a8886d2aff006c5116e6e17090194dcab08
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2019-11-26 19:19:45 +0200

    Play with macro # - stringify ## - concat.

--- c_fsm/main.c
+++ c_fsm/main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define f(a,b) a##b
+#define g(a) #a
+#define h(a) g(a)
+
 int main (int argc, const char * argv[]){
-  printf("It works:%d!!!\n", 100);
+  printf("Macro stringify:%s\n", g(f(1,2)));
+  printf("Macro concatenate:%s\n", h(f(1,2)));
   return 0;
 }
