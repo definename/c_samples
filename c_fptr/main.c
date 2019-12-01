a0c3b465b0161fad0d03987b897860dc719a9ee7
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2019-12-02 00:07:57 +0200

    Play with function pointer.

---
+++ c_fptr/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+typedef void (*message_t)(void);
+
+void hello(void) {
+  printf("Hello\n");
+}
+
+void goodbye(void) {
+  printf("Goodbye\n");
+}
+
+void call_hello_or_goodbye(message_t m) {
+  m();
+}
+
+int main (int argc, const char * argv[]) {
+  // .......................................
+  message_t mobj;
+
+  mobj = hello;
+  mobj();
+
+  mobj = goodbye;
+  mobj();
+
+  // .......................................
+  void* m_ptr = hello;
+  ((message_t)m_ptr)();
+
+  // .......................................
+  call_hello_or_goodbye(hello);
+  call_hello_or_goodbye(goodbye);
+
+  return 0;
+}
