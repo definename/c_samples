5a6e15eebf8435114776aae03188f1090e79a460
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2019-12-02 12:08:47 +0200

    Init const struct data.

--- c_struct/main.c
+++ c_struct/main.c
@@ -11,10 +11,18 @@ struct
   {2, 22}
 };
 
+typedef struct {
+  int i;
+  const int j;
+} test_t;
+
 int main (int argc, const char *argv[]){
   const size_t size = sizeof desc /sizeof desc[0];
   for (size_t i = 0; i < size; ++i){
     printf("%d %d\n", desc[i].a, desc[i].b);
   }
+
+  test_t t = {11, 22};
+  printf("ti: %d tj:%d\n", t.i, t.j);
   return 0;
 }
