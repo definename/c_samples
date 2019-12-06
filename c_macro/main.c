e1972a0eb67e27a6cd02bf913dde0d821ccbcbbd
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2019-12-06 12:33:19 +0200

    Play with macro to str.

--- c_macro/main.c
+++ c_macro/main.c
@@ -5,8 +5,33 @@
 #define g(a) #a
 #define h(a) g(a)
 
+#define ENUM_TO_STR(e) #e
+
+typedef enum {
+  ONE_ITEM,
+  TWO_ITEM,
+  ITEM_UNKNOWN
+} item_t;
+
+void test_enum_to_str(const item_t item) {
+  switch(item) {
+    case ONE_ITEM:
+    printf("%s\n", ENUM_TO_STR(ONE_ITEM));
+    break;
+    case TWO_ITEM:
+    printf("%s\n", ENUM_TO_STR(ONE_ITEM));
+    break;
+    default:
+    printf("%s\n", ENUM_TO_STR(ITEM_UNKNOWN));
+  }
+}
+
 int main (int argc, const char * argv[]){
   printf("Macro stringify:%s\n", g(f(1,2)));
   printf("Macro concatenate:%s\n", h(f(1,2)));
+
+  test_enum_to_str(ONE_ITEM);
+  test_enum_to_str(TWO_ITEM);
+  test_enum_to_str(99);
   return 0;
 }
