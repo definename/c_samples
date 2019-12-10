c825334fd08de94e27e40b6790b3ca2e6783da2b
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2019-12-11 00:47:06 +0200

    Bubble sort.

--- c_sort/main.c
+++ c_sort/main.c
@@ -1,33 +1,67 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void insert_sort(int* arr, const int size);
+void bubble_sort(int* arr, const int size);
+
 void print_arr(int* arr, const int size);
 
 int main (int argc, const char * argv[]) {
-  // Insertion sort.
   int arr[] = { 1, 2, 90, 4, 2, 2, 299, 3 };
   const int size = sizeof arr/sizeof(int);
-  insert_sort(arr, size);
-  print_arr(arr, size);
+
+  printf("Insertion sort.........\n");
+  int* arr_insert = malloc(size);
+  memcpy(arr_insert, arr, size * sizeof(int));
+  insert_sort(arr_insert, size);
+  free(arr_insert);
+
+  printf("Bubble sort.........\n");
+  int* arr_bubble = malloc(size);
+  memcpy(arr_bubble, arr, size * sizeof(int));
+  bubble_sort(arr_bubble, size);
+  free(arr_bubble);
 
   return 0;
 }
 
+void bubble_sort(int* arr, const int size) {
+  print_arr(arr, size);
+  for (int i = 0; i < size; ++i) {
+    for (int j = 0; j < size - 1; ++j) {
+      if (arr[j] > arr[j+1]) {
+        printf("swap %d %d\n", arr[j], arr[j+1]);
+        int tmp = arr[j+1];
+        arr[j+1] = arr[j];
+        arr[j] = tmp;
+        print_arr(arr, size);
+      }
+    }
+  }
+  print_arr(arr, size);
+}
+
 void insert_sort(int* arr, const int size) {
+  print_arr(arr, size);
   for (int i = 1; i < size; ++i) {
     int j = i;
     while (j > 0 && arr[j-1] > arr[j])
     {
+      printf("swap %d %d\n", arr[j], arr[j-1]);
       int tmp = arr[j-1];
       arr[j-1] = arr[j];
       arr[j] = tmp;
       --j;
+      print_arr(arr, size);
     }
   }
+  print_arr(arr, size);
 }
 
 void print_arr(int* arr, const int size) {
     for(int i = 0; i < size; ++i) {
       printf("%d ", arr[i]);
     }
+    printf("\n");
 }
