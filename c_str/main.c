82a86dec99d2c1323845b4d8d0748d45934c5ce8
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2019-12-11 22:55:04 +0200

    Define test name.

--- c_str/main.c
+++ c_str/main.c
@@ -1,20 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
+#define TEST_NAME "TEST_STR_NAME"
+
+void test_str_fn(const char * const name);
+
 int main (int argc, const char * argv[]) {
-
-  char* test_str;
-  test_str = NULL;
-  if (test_str) {
-    char* test_str_dup = strdup(test_str);
-    if (test_str_dup) {
-      printf("Test str dup is ok\n");
+    // ........................................
+    char* test_str = NULL;
+    if (test_str) {
+        char* test_str_dup = strdup(test_str);
+        if (test_str_dup) {
+            printf("Test str dup is ok\n");
+        } else {
+            printf("Test str dup is nok\n");
+        }
     } else {
-      printf("Test str dup is nok\n");
+        printf("Test str is null\n");
     }
-  } else {
-    printf("Test str is null\n");
-  }
+    // ........................................
+    test_str_fn(TEST_NAME);
+    return 0;
+}
 
-  return 0;
+void test_str_fn(const char * const name) {
+    char* name_copy = strdup(name);
+    printf("%s\n", name_copy);
+    free(name_copy);
 }
