2245f4f769c5198d075133d994740c0d67227b5e
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2019-12-14 00:56:17 +0200

    Play with strdup.

--- c_str/main.c
+++ c_str/main.c
@@ -4,28 +4,42 @@
 
 #define TEST_NAME "TEST_STR_NAME"
 
-void test_str_fn(const char * const name);
+void test_str_heap_copy(const char * const name);
+void test_str_heap_copy1(const char * const name);
+void test_strdup();
 
 int main (int argc, const char * argv[]) {
-    // ........................................
-    char* test_str = NULL;
+    test_str_heap_copy(TEST_NAME);
+    test_str_heap_copy1(TEST_NAME);
+    test_strdup(TEST_NAME);
+    test_strdup(NULL);
+    return 0;
+}
+
+void test_str_heap_copy(const char * const name) {
+    char* name_copy = strdup(name);
+    printf("%s\n", name_copy);
+    free(name_copy);
+}
+
+void test_str_heap_copy1(const char * const name) {
+    size_t size = strlen(name) + 1;
+    char* name_copy = malloc(size * sizeof(char));
+    strcpy(name_copy, name);
+    printf("%s\n", name_copy);
+    free(name_copy);
+}
+
+void test_strdup(char* test_str) {
     if (test_str) {
         char* test_str_dup = strdup(test_str);
         if (test_str_dup) {
-            printf("Test str dup is ok\n");
+            printf("Test str dup is ok:%p\n", test_str_dup);
+            free(test_str_dup);
         } else {
             printf("Test str dup is nok\n");
         }
     } else {
-        printf("Test str is null\n");
+        printf("Test str:%p is null\n", test_str);
     }
-    // ........................................
-    test_str_fn(TEST_NAME);
-    return 0;
-}
-
-void test_str_fn(const char * const name) {
-    char* name_copy = strdup(name);
-    printf("%s\n", name_copy);
-    free(name_copy);
 }
