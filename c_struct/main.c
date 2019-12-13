296aa9004da4d863f720726570acf85cbac6df61
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2019-12-14 00:15:13 +0200

    Init struct with macro.

--- c_struct/main.c
+++ c_struct/main.c
@@ -14,15 +14,30 @@ struct
 typedef struct {
   int i;
   const int j;
+  const char* name;
 } test_t;
 
-int main (int argc, const char *argv[]){
+#define TEST_DEFAULT \
+{ \
+  .i = 11, \
+  .j = 22, \
+  .name = "NAME", \
+} \
+
+int main (int argc, const char *argv[]) {
+  //.....................................................
   const size_t size = sizeof desc /sizeof desc[0];
-  for (size_t i = 0; i < size; ++i){
+  for (size_t i = 0; i < size; ++i) {
     printf("%d %d\n", desc[i].a, desc[i].b);
   }
 
+  //.....................................................
   test_t t = {11, 22};
   printf("ti: %d tj:%d\n", t.i, t.j);
+
+  //.....................................................
+  test_t test = TEST_DEFAULT;
+  printf("i:%d j:%d name:%s\n", test.i, test.j, test.name);
+
   return 0;
 }
