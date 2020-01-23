01c344296eeb91135b1d9d7aa4c353c8c2bdd13b
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2020-01-23 23:34:49 +0200

    protobuf support function, macro changes.

--- c_macro/main.c
+++ c_macro/main.c
@@ -5,20 +5,19 @@
 #define g(a) #a
 #define h(a) g(a)
 
-#define ENUM_TO_STR(e) #e
-
 typedef enum {
-  ONE_ITEM,
+  ONE_ITEM = 0,
   TWO_ITEM,
   ITEM_UNKNOWN
 } item_t;
 
+#define ENUM_TO_STR(e) #e
 #define CASE_TO_STR(x) case x: return ENUM_TO_STR(x);
+#define LOG_DEBUG(format, ...) printf(format, ##__VA_ARGS__)
 
-const char* item_to_str(int item) {
-  const char* ret;
+const char* item_to_str(item_t item) {
   switch(item) {
-    case ONE_ITEM: ENUM_TO_STR(ONE_ITEM);
+    CASE_TO_STR(ONE_ITEM)
     CASE_TO_STR(TWO_ITEM)
     default:
     return ENUM_TO_STR(ITEM_UNKNOWN);
@@ -26,12 +25,13 @@ const char* item_to_str(int item) {
 }
 
 int main (int argc, const char * argv[]){
-  printf("Macro stringify:%s\n", g(f(1,2)));
-  printf("Macro concatenate:%s\n", h(f(1,2)));
+  LOG_DEBUG("No data\n");
+  LOG_DEBUG("Data:%d\n", 1);
 
-  printf("..%s\n", ENUM_TO_STR(ONE_ITEM));
+  printf("MACRO stringify:%s\n", g(f(1,2)));
+  printf("MACRO concatenate:%s\n", h(f(1,2)));
 
-  for (int i = 0; i < 4; ++i) {
+  for (int i = 0; i < 3; ++i) {
     printf("%s(%d)\n", item_to_str(i), i);
   }
   return 0;
--- c_protobuf/main.c
+++ c_protobuf/main.c
@@ -7,143 +7,132 @@
 typedef Base base_msg_t;
 typedef Sub1 sub1_msg_t;
 
-void* pack_base(const int32_t a, const int32_t b, size_t *size);
-void* pack_sub1_msg(const int32_t val, size_t *size);
-
-void unpack_base(size_t* size, void *buff);
-void unpack_sub1_msg(size_t* size, void *buff);
-
-int main (int argc, const char * argv[]) {
-  size_t size;
-  void *buff = pack_base(111, 222, &size);
-  if (buff) {
-      unpack_base(&size, buff);
-
-      free(buff);
-      buff = NULL;
-  }
-
-  return 0;
-}
-
-void* pack_base(const int32_t a, const int32_t b, size_t *size) {
-  void *buff = NULL;
-  *size = 0;
-
+base_msg_t* build_base(const int32_t a, const int32_t b, void* sub1_msg, size_t sub1_size) {
   base_msg_t *msg = malloc(sizeof(base_msg_t));
   if (msg) {
       base__init(msg);
-
-      //..........................................
       msg->base_a = a;
+
       msg->has_base_b = true;
       msg->base_b = b;
 
-      printf("packed base a:%d\n", msg->base_a);
-      printf("packed base b:%d\n", msg->base_b);
-
-      size_t sub1_size;
-      void *sub1_buff = pack_sub1_msg(333, &sub1_size);
-      if (sub1_buff) {
+      if (sub1_msg) {
         msg->has_base_c = true;
-        msg->base_c.data = sub1_buff;
+        msg->base_c.data = sub1_msg;
         msg->base_c.len = sub1_size;
       }
-      //..........................................
-
-      size_t packed_size = base__get_packed_size(msg);
-      if (packed_size) {
-        buff = malloc(packed_size);
-        if (buff) {
-          base__pack(msg, buff);
-          *size = packed_size;
-          printf("Base has been successfully packed\n");
-        } else {
-          printf("Failed to allocate memory for base buffer\n");
-        }
-      } else {
-        printf("Failed to retrieve required packed size\n");
-      }
-
-      base__free_unpacked(msg, NULL);
-  } else {
-    printf("Failed to allocate protocol message\n");
   }
-  return buff;
+  return msg;
 }
 
-void* pack_sub1_msg(const int32_t sub_a, size_t *size) {
+void* pack_base(base_msg_t* base_msg, size_t* size) {
   void *buff = NULL;
   *size = 0;
 
-  sub1_msg_t *sub_msg = malloc(sizeof(sub1_msg_t));
-  if (sub_msg) {
-    sub1__init(sub_msg);
-
-    //..........................................
-    sub_msg->has_sub_a = true;
-    sub_msg->sub_a = sub_a;
-    printf("packed sub a:%d\n", sub_msg->sub_a);
-    //..........................................
-  
-    size_t packed_size = sub1__get_packed_size(sub_msg);
-    if (packed_size) {
-      buff = malloc(packed_size);
-      if (buff) {
-        sub1__pack(sub_msg, buff);
-        *size = packed_size;
-        printf("Sub1 has been successfully packed\n");
-      } else {
-        printf("Failed to allocate memory for sub1 buffer\n");
-      }
-    } else {
-      printf("Failed to retrieve required packed size\n");
+  size_t packed_size = base__get_packed_size(base_msg);
+  if (packed_size) {
+    buff = malloc(packed_size);
+    if (buff) {
+      base__pack(base_msg, buff);
+      *size = packed_size;
     }
-
-    sub1__free_unpacked(sub_msg, NULL);
-  } else {
-    printf("Failed to allocate protocol message\n");
   }
-
   return buff;
 }
 
-void unpack_base(size_t* size, void *buff) {
-  if (buff) {
-    base_msg_t *base_msg;
-    base_msg = base__unpack(NULL, *size, buff);
+sub1_msg_t* build_sub1_msg(const int32_t sub_a) {
+  sub1_msg_t *sub1_msg = malloc(sizeof(sub1_msg_t));
+  if (sub1_msg) {
+    sub1__init(sub1_msg);
 
-    //..........................................
-    printf("unpacked base a:%d\n", base_msg->base_a);
-    if (base_msg->has_base_b) {
-      printf("unpacked base b:%d\n", base_msg->base_b);
-    }
-    if (base_msg->has_base_c) {
-      unpack_sub1_msg(&base_msg->base_c.len, base_msg->base_c.data);
-    }
-    //..........................................
-
-    base__free_unpacked(base_msg, NULL);
+    sub1_msg->has_sub_a = true;
+    sub1_msg->sub_a = sub_a;
   }
-  else{
-    printf("Base buff is null\n");
+  return sub1_msg;
+}
+
+void* pack_sub1_msg(sub1_msg_t* sub1_msg, size_t *size) {
+  void *buff = NULL;
+  *size = 0;
+
+  size_t packed_size = sub1__get_packed_size(sub1_msg);
+  if (packed_size) {
+    buff = malloc(packed_size);
+    if (buff) {
+      sub1__pack(sub1_msg, buff);
+      *size = packed_size;
+    }
+  }
+  return buff;
+}
+
+base_msg_t *unpack_base(const size_t size, void *buff) {
+  base_msg_t *base_msg = NULL;
+  if (buff) {
+    base_msg = base__unpack(NULL, size, buff);
+  }
+  return base_msg;
+}
+
+sub1_msg_t *unpack_sub1_msg(const size_t size, void *buff) {
+  sub1_msg_t *sub1_msg = NULL;
+  if (buff) {
+    sub1_msg = sub1__unpack(NULL, size, buff);
+  }
+  return sub1_msg;
+}
+
+void base_dump(const base_msg_t *base_msg) {
+  if (base_msg) {
+    printf("base a:%d\n", base_msg->base_a);
+    if (base_msg->has_base_b) {
+      printf("base b:%d\n", base_msg->base_b);
+    }
   }
 }
 
-void unpack_sub1_msg(size_t* size, void *buff) {
-  if (buff) {
-    sub1_msg_t *sub1_msg;
-    sub1_msg = sub1__unpack(NULL, *size, buff);
-
-    //..........................................
+void sub1_dump(const sub1_msg_t *sub1_msg) {
+  if (sub1_msg) {
     if (sub1_msg->has_sub_a) {
-      printf("unpacked sub a:%d\n", sub1_msg->sub_a);
+      printf("sub a:%d\n", sub1_msg->sub_a);
     }
-    //..........................................
-
-    sub1__free_unpacked(sub1_msg, NULL);
   }
-  else{
-    printf("Sub1 buff is null\n");
+}
+
+void test_pack_unpack();
+
+int main (int argc, const char * argv[]) {
+  test_pack_unpack();
+  return 0;
+}
+
+void test_pack_unpack() {
+  int count = 100;
+  while (count)
+  {
+    sub1_msg_t* sub1_msg_ = build_sub1_msg(0);
+    size_t sub1_size;
+    void *sub1_buff = pack_sub1_msg(sub1_msg_, &sub1_size);
+    sub1__free_unpacked(sub1_msg_, NULL);
+
+    base_msg_t *base_msg_ = build_base(1, 2, sub1_buff, sub1_size);
+
+    size_t base_size;
+    void* base_buff = pack_base(base_msg_, &base_size);
+    base__free_unpacked(base_msg_, NULL);
+
+    base_msg_t *_base_msg = unpack_base(base_size, base_buff);
+    sub1_msg_t *_sub1_msg = unpack_sub1_msg(_base_msg->base_c.len, _base_msg->base_c.data);
+
+    sub1_dump(_sub1_msg);
+    base_dump(_base_msg);
+
+    sub1__free_unpacked(_sub1_msg, NULL);
+    base__free_unpacked(_base_msg, NULL);
+
+    free(base_buff);
+    base_buff = NULL;
+
+    count--;
   }
 }
