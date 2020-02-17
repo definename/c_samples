d7c146dd7d09089c004286cfcae1bff9774ddbf6
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2020-02-17 23:54:29 +0200

    fgets/fputs.

---
+++ c_io/main.c
@@ -0,0 +1,6 @@
+#include "test_fgets.h"
+
+int main (int argc, const char *argv[]) {
+  test_fgets();
+  return 0;
+}
---
+++ c_io/test_fgets.h
@@ -0,0 +1,6 @@
+#ifndef TEST_FGETS_H
+#define TEST_FGETS_H
+
+void test_fgets(void);
+
+#endif // TEST_FGETS_H
