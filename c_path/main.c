ef462b1e9f785f7b3820bad900f098cb8bff700f
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2020-03-18 00:09:14 +0200

    test simplify.

--- c_path/main.c
+++ c_path/main.c
@@ -1,5 +1,6 @@
 #include "test_util.h"
 
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
@@ -7,7 +8,7 @@
 #include <limits.h>
 #include <libgen.h>
 
-#define CWD "/home/olehk/projects/c_samples/c_path"
+#define CWD "/root/xxx/yyy/zzz"
 
 static char* get_cwd(void) {
     char *cwd = malloc(PATH_MAX);
@@ -19,59 +20,128 @@ static char* get_cwd(void) {
     }
 }
 
-int main (int argc, const char * argv[]) {
-    const char *dir_name = "filename";
-
-    char *name_copy = malloc(PATH_MAX);
-    strcpy(name_copy, dir_name);
-    log_debug("Name copy:%s\n", name_copy);
-
-    char* bname = basename(name_copy);
-    if (strcmp(bname, "..") == 0 || strcmp(bname, ".") == 0 || strcmp(bname, "/") == 0) {
-        log_debug("NOK base name:%s\n", bname);
-    } else {
-        log_debug("OK base name:%s\n", bname);
-
-        char* dir_copy = malloc(PATH_MAX);
-        strcpy(dir_copy, dir_name);
-        log_debug("Dir copy:%s\n", dir_copy);
-        char *dname = dirname(dir_copy);
-        log_debug("Dir name:%s\n", dname);
-
-        char *cwd = malloc(PATH_MAX);
-        if (cwd) {
-            strcpy(cwd, CWD);
-            strcat(cwd, "/");
-            strcat(cwd, dname);
-            log_debug("Resolve:%s\n", cwd);
-            char *resolved = realpath(cwd, NULL);
-            if (resolved) {
-                log_debug("Resolved:%s\n", resolved);
-
-                size_t s = strlen(CWD);
-                log_debug("Size:%zu\n", s);
-                if (strncmp(CWD, resolved , s) == 0) {
-                    log_debug("Name OK\n");
+static char* simplify_path(const char* const path) {
+    char *reverse = NULL;
+    char *tmp = NULL;
+    if (tmp = strdup(path)) {
+        char *dir = NULL;
+        if (dir = malloc(PATH_MAX)) {
+            char *to_reverse = NULL;
+            if (to_reverse = malloc(PATH_MAX)) {
+                if (*path == '/') {
+                    strcpy(to_reverse, "/");
                 } else {
-                    log_debug("Name NOK\n");
+                    strcpy(to_reverse, "");
                 }
 
-                free(resolved);
-                resolved = NULL;
+                char* p = NULL;
+                int skip = 0;
+                do {
+                    if (p = strrchr(tmp, '/')) {
+                        char *cur = p + 1;
+                        if (strcmp(cur, ".") != 0 && strcmp(cur, "") != 0) {
+                            if (strcmp(cur, "..") == 0) {
+                                skip++;
+                            } else if (skip != 0) {
+                                --skip;
+                            } else if (strcmp(dir, "") == 0) {
+                                strcpy(dir, cur);
+                            } else {
+                                strcat(to_reverse, dir);
+                                strcat(to_reverse, "/");
+                                *dir = '\0';
+                                strcpy(dir, cur);
+                            }
+                        }
+                        *p = '\0';
+                    }
+                } while(p);
+
+                if (dir && strcmp(dir, "") != 0) {
+                    strcat(to_reverse, dir);
+                    strcat(to_reverse, "/");
+                }
+
+                if (skip == 0 && tmp && strcmp(tmp, "") != 0) {
+                    strcat(to_reverse, tmp);
+                    strcat(to_reverse, "/");
+                }
+
+                if (reverse = malloc(PATH_MAX)) {
+                    if (*path == '/') {
+                        strcpy(reverse, "/");
+                    } else {
+                        strcpy(reverse, "");
+                    }
+
+                    log_debug("To reverse:%s\n", to_reverse);
+                    p = NULL;
+                    do {
+                        if (p = strrchr(to_reverse, '/')) {
+                            char *cur = p + 1;
+                            if (strcmp(cur, "") != 0) {
+                                strcat(reverse, cur);
+                                strcat(reverse, "/");
+                            }
+                            *p = '\0';
+                        }
+                    } while(p);
+
+                    if (to_reverse && strcmp(to_reverse, "") != 0) {
+                        strcat(reverse, to_reverse);
+                        strcat(to_reverse, "/");
+                    }
+                } else {
+                    log_debug("Failed to alloc reverse string\n");
+                }
+                free(to_reverse);
+                to_reverse = NULL;
             } else {
-                log_debug("Failed to resolve\n");
+                log_debug("Failed to alloc to reverse string\n");
             }
-            free(cwd);
-            cwd = NULL;
+            free(dir);
+            dir = NULL;
         } else {
-            log_debug("Failed to alloc cwd");
+            log_debug("Failed to alloc tmp string\n");
+        }
+        free(tmp);
+        tmp = NULL;
+    } else {
+        log_debug("Failed to duplicate tmp string\n");
+    }
+    return reverse;
+}
+
+int main (int argc, const char * argv[]) {
+    char* test1 = malloc(PATH_MAX);
+    const char *test1_res = "/root/xxx/dir1/dir2/dir3/";
+// #define CWD "/../../../../../a"
+// #define CWD "/a/.."
+// #define CWD "/a/./b/../../c/"
+// #define CWD "/a/.."
+// #define CWD "/home"
+    if (test1) {
+        strcpy(test1, CWD);
+        strcat(test1, "/");
+        strcat(test1, "../../dir1/dir2/dir3");
+
+        for (int i = 0; i != 100; ++i) {
+            log_debug("Cwd:%s\n", STRSAFE(test1));
+            char *test1_res = simplify_path(test1);
+            log_debug("Test1 res:%s\n", STRSAFE(test1_res));
+            if (strcmp(test1_res, "")) {
+                log_debug("TEST1 OK\n");
+            } else {
+                log_debug("TEST1 NOK");
+            }
+            free(test1_res);
+            test1_res = NULL;
         }
 
-        free(dir_copy);
-        dir_copy = NULL;
+        free(test1);
+        test1 = NULL;
+    } else {
+        log_debug("Failed to alloc test\n");
     }
-
-    free(name_copy);
-    name_copy = NULL;
     return 0;
 }
--- test_util.h
+++ test_util.h
@@ -3,7 +3,8 @@
 
 #include <stdio.h>
 
-#define COUNT_OF(arr) (sizeof(arr)/sizeof(arr[0]))
 #define log_debug(format, ...) printf(format, ##__VA_ARGS__)
+#define COUNT_OF(arr) (sizeof(arr) / sizeof(arr[0]))
+#define STRSAFE(s) ((s) ? (s) : "(null)")
 
 #endif // TEST_UTIL_H
