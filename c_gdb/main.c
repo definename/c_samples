25b2efd324a0735f28bea97c8be3edc3c8906bc8
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2020-08-27 22:49:08 +0300

    Insert into char array

--- c_gdb/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdbool.h>
-
-int main (int argc, const char * argv[]) {
-  int i = 100;
-  printf("It works!!!\n");
-  return 0;
-}
--- test_makefile/main.c
+++ test_makefile/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <stdint.h>
 
 int main(int argc, char* argv[]) {
-    printf("%d\n", 10);
+    char arr[] = "0123456789abcd";
+    uint32_t c = 0x55555555;
+    *((uint32_t*)arr + 1) = c;
+
+    printf("%s\n", arr);
     return 0;
 }
